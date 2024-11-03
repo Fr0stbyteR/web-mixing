f1ac39370c4669c6cc66bde19e1b3529fad2250a
shren <renshihong@hotmail.com>
2024-11-04 02:34:54 +0800

    add loading

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ const AUDIO_CONTEXT = new AudioContext({ latencyHint: 0.0001 });
 const App: React.FunctionComponent<Props> = ({ repositoryUrl = REPOSITORY_URL, audioContext = AUDIO_CONTEXT }) => {
     const [audioEditor, setAudioEditor] = useState<AudioEditor | null>(null);
     const [questData, setQuestData] = useState<QuestData[string] | null>(null);
+    const [error, setError] = useState<string | null>(null);
+    const [loaded, setLoaded] = useState(0);
+
     const quest = new URLSearchParams(location.search).get("q");
     useEffect(() => {
         (async () => {
@@ -27,12 +30,15 @@ const App: React.FunctionComponent<Props> = ({ repositoryUrl = REPOSITORY_URL, a
     }, [quest, repositoryUrl]);
     useEffect(() => {
         (async () => {
+            if (!quest) setError("Data not found");
             if (!quest || !questData) return;
+            setError(null);
             const { files, path, pans } = questData;
             const audioBuffers = await Promise.all(files.map(async (fileName) => {
                 const url = new URL(`${path}/${fileName}`, repositoryUrl);
                 const response = await fetch(url);
                 const arrayBuffer = await response.arrayBuffer();
+                setLoaded(v => v + 1);
                 return audioContext.decodeAudioData(arrayBuffer);
             }));
             const audioEditor = await AudioEditor.fromData(audioBuffers.map(ab => ab.getChannelData(0)), audioContext, quest);
@@ -55,11 +61,20 @@ const App: React.FunctionComponent<Props> = ({ repositoryUrl = REPOSITORY_URL, a
     }, [audioContext, quest, questData, repositoryUrl])
     return (
         <div id="main-container">
-            {audioEditor ? (
-                <AudioEditorContext.Provider value={audioEditor}>
-                    <AudioEditorContainer />
-                </AudioEditorContext.Provider>
-            ) : <VSCodeProgressRing />}
+            {
+                audioEditor
+                ? (
+                    <AudioEditorContext.Provider value={audioEditor}>
+                        <AudioEditorContainer />
+                    </AudioEditorContext.Provider>
+                )
+                : error
+                ? <div className="main-loading-error">{error}</div>
+                : <div className="main-loading">
+                    <VSCodeProgressRing />
+                    <div>Loading... {questData ? `${loaded} / ${questData.files.length}` : ""}</div>
+                </div>
+            }
         </div>
     );
 };
